718a6348df554e43397b7256d5f5a8296ab074b8
Aaron21Male <aaronallezra@gmail.com>
2026-02-05 16:43:06 +0100

    Set up Postgresql database connection with sqlalchemy

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Declarativebase
+
+# Load environment variables from .env file
+load_dotenv() 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+      raise ValueError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
