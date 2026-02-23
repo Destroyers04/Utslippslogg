11dbcaf4b3a85d61aff02f0d666c4a1e02d83473
Aaron21Male <aaronallezra@gmail.com>
2026-02-23 10:59:17 +0100

    Renaming primary key for better clarity

--- backend/schemas/station.py
+++ backend/schemas/station.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 # Every site has a name and location
 class StationBase(BaseModel):
     name: str
-    location_description: str # Where the station is located on the premises
+    location_description: str 
 
 class CreateStation(StationBase):
-    site_id: int
+    site_id: int #foreign key to site table
 
 class StationResponse(StationBase):
-    id: int
-    site_id:int
+    station_id: int #foreign key to station table
+    site_id:int #foreign key to site table
 
     class Config:
         from_attributes = True
