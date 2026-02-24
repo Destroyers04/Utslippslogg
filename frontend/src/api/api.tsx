362dc50dfcb0199277105e5e0da077a7f8e582bc
Aaron21Male <aaronallezra@gmail.com>
2026-02-24 18:01:48 +0100

    Added api folder

---
+++ frontend/src/api/api.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import type { Token } from "./types";
+
+// Check API.md or "http://127.0.0.1:8000/docs#" for more information on the API endpoints and expected responses.
+const API_URL = "http://127.0.0.1:8000";
+
+export const getLogInToken = async (
+  email: string,
+  password: string,
+): Promise<Token> => {
+  try {
+    // OAuth2 requires form data, not JSON
+    const params = new URLSearchParams();
+    params.append("username", email);
+    params.append("password", password);
+    const response = await axios.post(`${API_URL}/auth/token`, params);
+    const accessToken = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    throw error;
+  }
+};
