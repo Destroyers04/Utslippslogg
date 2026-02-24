f2f7d8bc208ca5b8223e324c29d1880260678f97
Aaron21Male <aaronallezra@gmail.com>
2026-02-24 17:11:45 +0100

    Added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,17 @@ from database import engine
 from models import site, measurement, userData
 from routers import user, get, post
 from dependencies import db_dependency, user_dependency
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(get.router)
