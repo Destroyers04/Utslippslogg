2d42dad064f2605619b7d62d0432b8a8567a345d
Aaron21Male <aaronallezra@gmail.com>
2026-02-24 18:40:10 +0100

    Added toaster

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "@/components/ui/sonner";
 
 const RootLayout = () => (
   <>
     <Outlet />
+    <Toaster />
     <TanStackRouterDevtools />
   </>
 );
