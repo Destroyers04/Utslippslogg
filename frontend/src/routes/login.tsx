afa70f9f397ed6c5e45f66d0c1376322ce3509e1
Aaron21Male <aaronallezra@gmail.com>
2026-02-25 11:25:24 +0100

    Changed a to link

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, redirect, Link } from "@tanstack/react-router";
 import { GalleryVerticalEnd } from "lucide-react";
 import mountains from "@/assets/mountains.jpg";
 import { LoginForm } from "@/components/login-form";
@@ -20,12 +20,15 @@ function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <Link
+            to={dashboardRoute.to}
+            className="flex items-center gap-2 font-medium"
+          >
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
             Utslippslogg
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
