20aa163ebcbf243f57191d1726b25c9dfe949e04
Aaron21Male <aaronallezra@gmail.com>
2026-03-02 17:15:49 +0100

    Created cards and dynamic card lister

--- frontend/src/components/site-cards.tsx
+++ frontend/src/components/site-cards.tsx
@@ -1,16 +1,67 @@
-import { useQuery } from "@tanstack/react-query";
-import { getSiteData } from "@/api/api";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { MapPin, Cctv } from "lucide-react";
 
-function SiteCards() {
-  const { isPending, error, siteData } = useQuery({
-    queryKey: ["siteData"],
-    queryFn: () => getSiteData(localStorage.getItem("token")),
-  });
+interface Props {
+  site_id: number;
+  site_name: string;
+  site_location: string;
+  station_count: number;
+  active: boolean;
+}
 
-  if (isPending) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-  return <div className="max-w-screen-xl mx-auto mt-8 px-8">Hello</div>;
+function SiteCards({
+  site_id,
+  site_name,
+  site_location,
+  station_count,
+  active,
+}: Props) {
+  return (
+    <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+      <img
+        src={`https://picsum.photos/seed/${site_id}/400/200`}
+        alt="Image of site"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge
+            variant={active ? "secondary" : "destructive"}
+            className={active ? "bg-green-300" : "bg-red-300"}
+          >
+            {active ? "Operating" : "Inactive"}
+          </Badge>
+        </CardAction>
+        <CardTitle>{site_name}</CardTitle>
+        <CardDescription>
+          <div className="flex items-center gap-1">
+            <MapPin className="h-4 w-4" />
+            {site_location}
+          </div>
+        </CardDescription>
+        <CardDescription>
+          <div className="flex items-center gap-1">
+            <Cctv className="h-4 w-4" />
+            {station_count} stations
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Site</Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export { SiteCards };
--- frontend/src/components/site-collection.tsx
+++ frontend/src/components/site-collection.tsx
@@ -1,8 +1,34 @@
 import { SiteCards } from "@/components/site-cards";
+import { useQuery } from "@tanstack/react-query";
+import { getSiteData } from "@/api/api";
+import type { SiteData } from "@/api/types";
+
 function SiteCollection() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["siteData"],
+    queryFn: () => getSiteData(localStorage.getItem("token")!),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <>
-      <SiteCards />
+      <div className="max-w-screen-xl mx-auto mt-8 px-8">
+        {data.map((site) => {
+          return (
+            <SiteCards
+              key={site.site_id}
+              site_id={site.site_id}
+              site_name={site.name}
+              site_location={site.location}
+              station_count={site.station_count}
+              active={true}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
